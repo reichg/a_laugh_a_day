d8bc38ebbd40217638c0fbfc2ab17f934345415e
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2023-03-14 03:51:43 -0700

    adding joke limitation and subscription/entitlement

---
+++ lib/widgets/subscription_page/subscription.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SubscriptionsPage extends StatefulWidget {
+  const SubscriptionsPage({Key? key}) : super(key: key);
+
+  @override
+  State<SubscriptionsPage> createState() => _SubscriptionsPageState();
+}
+
+class _SubscriptionsPageState extends State<SubscriptionsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Out Of Jokes Today!"),
+      content: Text("Content"),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import purchases_flutter
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
 }
